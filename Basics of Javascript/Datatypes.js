a9ba7ed8d269cc7486f18134ab34586234c73068
Gupta-Ravi <raviramsagar2441@gmail.com>
2024-11-10 12:00:03 +0000

    Data type defined

--- Basics of Javascript/Datatypes.js
+++ Basics of Javascript/Datatypes.js
@@ -1,1 +1,6 @@
-console.log("Hello World")
+const accountId = "roshan-yadav@gmail.com"
+let accountName = "Roshan Yadav"
+var accountPassword = "123@abc.com"
+
+console.log(accountId);
+console.table([accountId, accountName, accountPassword])
