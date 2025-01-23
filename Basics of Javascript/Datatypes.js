2b1cf12f029a32376fff23648a43168a33876e45
Gupta-Ravi <177751776+Gupta-Ravi@users.noreply.github.com>
2025-01-23 16:31:08 +0000

    Symbol Declearation in Object

--- Basics of Javascript/Datatypes.js
+++ Basics of Javascript/Datatypes.js
@@ -1,9 +1,12 @@
+let Sym = Symbol('Mysymbol1')
 let userDetails = {
     name: "Ravi Gupta",
     email: "ravigupta@gmail.com",
     phone: 9004592315, 
     age: 22,
-    "greet": "Hello World!"
+    "greet": "Hello World!",
+    [Sym]: 56
 };
 
-console.log(`User Name:- ${userDetails.name} Greet:- ${userDetails["greet"]}`);
+console.log(`Type of symbol :- ${userDetails['Sym']}`)
+console.log(`User Name:- ${userDetails['email']} Greet:- ${userDetails["greet"]}`);
