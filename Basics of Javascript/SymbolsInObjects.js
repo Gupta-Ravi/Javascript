5ebcb021f11ac3b93115a8a1a6b6bcf904f37877
Gupta-Ravi <177751776+Gupta-Ravi@users.noreply.github.com>
2025-02-23 18:30:34 +0000

    Add detailed description about the code

--- Basics of Javascript/SymbolsInObjects.js
+++ Basics of Javascript/SymbolsInObjects.js
@@ -1,4 +1,10 @@
-let name = Symbol('Name');
+//Reasone - 
+/* We have defined symbol here because if we extract details of say 10 or 12 users from the database then we would have to create 5 variables for each user but by using symbol we just have to create a variable for users. examples - user1, user2, user3..., user12. */
+
+
+//Code - 
+
+/* let name = Symbol('Name');
 let age = Symbol('Age');
 let email = Symbol('Email');
 let password = Symbol('Password');
@@ -21,4 +27,22 @@ let user2 = {
 }
 
 console.log(user2);
-console.log(`User Name : ${user2[name]}, Age : ${user2[age]}, Email : ${user2[email]}`)
+console.log(`User Name : ${user2[name]}, Age : ${user2[age]}, Email : ${user2[email]}`) */
+
+let user1 = {
+    name: 'Ravi Gupta',
+    age: 32,
+    email: 'ravigupta@gamil.com',
+    passoword: 'RaviGupta12'
+}
+
+console.log(`User Name - ${user1.name}, Age : ${user1.age}, Email : ${user1.email}`)
+
+let user2 = {
+    name: 'Aujun Viswakarma',
+    age: 25,
+    email: 'arjunviswakarma@gamil.com',
+    passoword: 'Arjun12'
+}
+
+console.log(`User Name - ${user2.name}, Age : ${user2.age}, Email : ${user2.email}`)
