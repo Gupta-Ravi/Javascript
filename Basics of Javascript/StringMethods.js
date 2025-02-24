7be42e9ed7b2208ee780f51c8e79c9ebbde4047d
Gupta-Ravi <177751776+Gupta-Ravi@users.noreply.github.com>
2025-02-25 01:37:41 +0530

    Update StringMethods.js

--- Basics of Javascript/StringMethods.js
+++ Basics of Javascript/StringMethods.js
@@ -1,7 +1,8 @@
-let userName = 'Ravi ravi Gupta';
+let userName = 'Ravi Gupta';
 let searchChar = 'Ravi';
 let changeChar = 'Kunal';
 
+//Here gi = g for search the character in all string and i for case in-sensitive
 let regExp = new RegExp(searchChar, 'gi');
 let newUserName = userName.replace(regExp, changeChar);
 console.log(`New User Name is ${newUserName}`);
